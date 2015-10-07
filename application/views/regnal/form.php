2d8801b8efa669893bac735606e765b156508fa6
Luis Felipe Perez <slashcat@gmail.com>
2015-10-06 23:13:08 -0500

    vista base para regnal

--- application/views/regnal/form.php
+++ application/views/regnal/form.php
@@ -32,6 +32,58 @@
             <?php echo $error; ?>
           </div>
 
+          <?php $error = form_error('data[provincia]'); ?>
+          <div class="form-group<?php echo ($error != '') ? ' has-error' : ''; ?>">
+            <label for="provincia" class="col-lg-2 control-label">Provincia</label>
+            <div class="col-lg-3">
+              <input type="text" name="data[provincia]" id="provincia" class="form-control" value="<?php echo set_value('provincia', $provincia); ?>">
+            </div>
+            <?php echo $error; ?>
+          </div>
+
+          <?php $error = form_error('data[localidad]'); ?>
+          <div class="form-group<?php echo ($error != '') ? ' has-error' : ''; ?>">
+            <label for="localidad" class="col-lg-2 control-label">Localidad</label>
+            <div class="col-lg-3">
+              <input type="text" name="data[localidad]" id="localidad" class="form-control" value="<?php echo set_value('localidad', $localidad); ?>">
+            </div>
+            <?php echo $error; ?>
+          </div>
+
+          <?php $error = form_error('data[grupo]'); ?>
+          <div class="form-group<?php echo ($error != '') ? ' has-error' : ''; ?>">
+            <label for="grupo" class="col-lg-2 control-label">Grupo</label>
+            <div class="col-lg-3">
+              <input type="text" name="data[grupo]" id="grupo" class="form-control" value="<?php echo set_value('grupo', $grupo); ?>">
+            </div>
+            <?php echo $error; ?>
+          </div>
+
+          <?php $error = form_error('data[nivel]'); ?>
+          <div class="form-group<?php echo ($error != '') ? ' has-error' : ''; ?>">
+            <label for="nivel" class="col-lg-2 control-label">Nivel</label>
+            <div class="col-lg-3">
+              <input type="text" name="data[nivel]" id="nivel" class="form-control" value="<?php echo set_value('nivel', $nivel); ?>">
+            </div>
+            <?php echo $error; ?>
+          </div>
+
+          <?php $error = form_error('data[vigencia]'); ?>
+          <div class="form-group<?php echo ($error != '') ? ' has-error' : ''; ?>">
+            <label for="vigencia" class="col-lg-2 control-label">Vigencia</label>
+            <div class="col-lg-3">
+              <input type="text" name="data[vigencia]" id="vigencia" class="form-control" value="<?php echo set_value('vigencia', $vigencia); ?>">
+            </div>
+            <?php echo $error; ?>
+          </div>
+
+          <div class="form-group">
+            <div class="col-lg-3 col-sm-offset-2 btn-crud">
+              <button type="submit" class="btn btn-success">Guardar</button>
+              <a href="/regnal" class="btn btn-default">Regresar</a>
+            </div>
+          </div>
+
         </form>
       </div>
     </section>
