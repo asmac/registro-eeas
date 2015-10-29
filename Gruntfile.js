118e874a1837ac27e1fb02136c6cbfcda5d2cf16
Luis Felipe Perez <slashcat@gmail.com>
2015-10-29 13:38:41 -0600

    agregar carpeta views a monitor de grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,7 +30,7 @@ module.exports = function(grunt) {
     },
     watch: {
       layout: {
-        files: ['*.html', 'assets/js/*.js'],
+        files: ['application/views/**/*.php', 'assets/js/*.js'],
         options: {
           livereload: true
         }
