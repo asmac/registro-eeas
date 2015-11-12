0de2f48d11dfd7c34465f07558102412039b8332
Luis Felipe Perez <slashcat@gmail.com>
2015-11-12 01:57:23 -0500

    cifras en panel de inicio

--- application/controllers/Panel.php
+++ application/controllers/Panel.php
@@ -9,7 +9,16 @@ class Panel extends MY_Controller {
 
 	public function index()
 	{
+		$data = array(
+			'regnal'     => $this->db->count_all('regnal'),
+			'registered' => $this->db->where('id_camping >', '0')->count_all_results('attendees'),
+			'left'       => $this->db->where('id_camping', '0')->count_all_results('attendees'),
+			'campings'   => $this->db->count_all('campings'),
+			'switches'   => $this->db->where('switch !=', '')->count_all_results('attendees')
+		);
+
 		$this->template->write('title', 'Panel');
+		$this->template->write_view('content', 'admin/index', $data);
 		$this->template->render();
 	}
 
--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,5 +1,83 @@
-<div class="row mg-b">
-  <div class="col-sm-12">
-    <h3 class="no-margin">Dashboard</h3>
-  </div>
-</div>
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Eventos Nacionales</h1>
+
+        <ol class="breadcrumb">
+          <li class="active">Inicio</li>
+        </ol>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="panel">
+          <div class="panel-body">
+            <div class="circle-icon bg-primary">
+              <i class="fa fa-users"></i>
+            </div>
+            <div>
+              <h3 class="no-margin"><?php echo number_format($regnal) ?></h3>
+              Miembros Vigentes
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="panel">
+          <div class="panel-body">
+            <div class="circle-icon bg-success">
+              <i class="fa fa-check"></i>
+            </div>
+            <div>
+              <h3 class="no-margin"><?php echo number_format($registered) ?></h3>
+              Asistentes Registrados
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="panel">
+          <div class="panel-body">
+            <div class="circle-icon bg-danger">
+              <i class="fa fa-clock-o"></i>
+            </div>
+            <div>
+              <h3 class="no-margin"><?php echo number_format($left) ?></h3>
+              Faltan por Llegar
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="panel">
+          <div class="panel-body">
+            <div class="circle-icon bg-info">
+              <i class="fa fa-map-signs"></i>
+            </div>
+            <div>
+              <h3 class="no-margin"><?php echo number_format($campings) ?></h3>
+              Áreas de Acampado
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="panel">
+          <div class="panel-body">
+            <div class="circle-icon bg-warning">
+              <i class="fa fa-exchange"></i>
+            </div>
+            <div>
+              <h3 class="no-margin"><?php echo number_format($switches) ?></h3>
+              Permutas Realizadas
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
